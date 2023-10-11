cf7f2b06842fb32d464a7d02838f6a5fcd9e02c3
Mikael Uhl <mikaeluhl@gmail.com>
2023-10-11 18:15:15 -0300

    Finance GET route created; Changing file permissions

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-import app from "./src/app.js";
+import express from "express";
+import * as bankController from "./src/controllers/bankController.js";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const app = express();
+
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => res.status(200).send("API Bancária"));
+
+app.get("/banks", bankController.getAllBanks);
+
+app.get("/banks/:id", bankController.getBankById);
+
+app.post("/finance", bankController.calculaAutoLoan);
+
+app.get("/finance", (req, res) => res.sendFile(path.join(__dirname, "public", "finance.html")));
+
+app.get("/favicon.ico", (req, res) => res.sendStatus(204));
 
 const port = 3000;
 
 app.listen(port, () => console.log("Servidor escutando em http://localhost:" + port));
+
+export default app;
